f1c7f89f0e14ce124d93a334cb7f7d7522143198
unknown <nivedhyakp9744610020@gmail.com>
2024-06-07 14:32:37 +0530

    student app

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <Navbar/>
+     <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                     <label htmlFor="" className="form-label">NAME</label>
+                     <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">ADMISSION NO</label>
+                     <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">ROLL NO</label>
+                     <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">PARENT NAME</label>
+                     <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">COLLEGE</label>
+                     <textarea name="" id="" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">DOB</label>
+                    <input type="date" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">EMAIL ID</label>
+                    <input type="email" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">PASSWORD</label>
+                    <input type="password" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">CONFIRM PASSWORD</label>
+                    <input type="password" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <button className="btn btn-success">REGISTER</button>
+                    </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default AddStudent
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">MEGHANA</li>
+                            <li class="list-group-item">SINGING</li>
+                            <li class="list-group-item">THRISSUR</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ORWEL</li>
+                            <li class="list-group-item">KOTTAYAM</li>
+                            <li class="list-group-item">CODING</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">RITHWIK</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">IDUKKI</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ORWEL</li>
+                            <li class="list-group-item">KOTTAYAM</li>
+                            <li class="list-group-item">CODING</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">RITHWIK</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">IDUKKI</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">SIVA</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">ERNAKULAM</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">SIVA</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">ERNAKULAM</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">SIVA</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">ERNAKULAM</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">SIVA</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">ERNAKULAM</li>
+                         </ul>
+                        </div> 
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">SIVA</li>
+                            <li class="list-group-item">DANCING</li>
+                            <li class="list-group-item">ERNAKULAM</li>
+                         </ul>
+                        </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAll
